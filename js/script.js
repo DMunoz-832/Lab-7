b0e97673ea67b1b3863fe2a9e78de1af756195d0
Daniela Munoz <danielamun.832@gmail.com>
2022-04-20 14:37:03 -0500

    corrected-connection-link

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 function init(){
-    //add your javascrip between these two lines of code
+    //add your javascript between these two lines of code
       function alertFunction(){
         var x1 = document.getElementById("entryinput").value;
         var x2 = document.getElementById("textoutput");
